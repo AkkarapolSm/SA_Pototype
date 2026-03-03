3be6788ef9d221744e3b3fb7e364030f1735acbe
Akkarapol Simlee <akkarapolsimlee11@gmail.com>
2026-03-04 06:30:03 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/SA_Pototype/',
   plugins: [
     react(),
     tailwindcss(),
